ad8c2ec1891d4573fef58ac8b00586adf239913a
Shoaib Khalid <shoaib.khalid@itcurves.com>
2025-09-01 16:45:17 +0500

    update for now

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at port ${process.env.PORT}`);
 });
+// app.use("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+// });
